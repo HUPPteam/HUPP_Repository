26da94d91e81554eb1726d345b093f372d3808a5
xiaojuejiang <370208645@qq.com>
2018-11-01 21:26:56 +0800

    计算器功能

---
+++ xulei/cals-xl-juejiang/lib/main.js
@@ -0,0 +1,11 @@
+const add=require('./add.js')
+const sub=require('./sub.js')
+const mul=require('./mul.js')
+const divide=require('./divide.js')
+
+module.exports={
+    add,
+    sub,
+    mul,
+    divide
+}
